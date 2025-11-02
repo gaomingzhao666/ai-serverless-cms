4f44f560503abebc872970a0eb4c9a13b73a66e7
Nano <gaomingzhao666@outlook.com>
2025-11-02 18:57:10 +0900

    chore: remove seo support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: "en",
-      },
-      title: "AI Serverless CMS",
-      meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { charset: "utf-8" },
-        {
-          name: "description",
-          content: "An AI-powered serverless content management system.",
-        },
-        {
-          name: "keywords",
-          content: "AI, CMS, Serverless, Content Management",
-        },
-        { name: "author", content: "Your Name" },
-      ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    },
-  },
+	compatibilityDate: '2025-07-15',
+	ssr: true,
 
-  modules: [
-    "@nuxt/content",
-    "@nuxt/image",
-    "@nuxt/test-utils",
-    "@nuxt/ui",
-    "@nuxtjs/seo",
-  ],
+	content: {
+		experimental: { nativeSqlite: true },
+	},
+
+	modules: ['@nuxt/content', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui'],
 })
